e943692c5090ecbf23acff82e6b0c82592f57fe6
lovemooner <lovemooner@yeah.net>
2017-05-03 13:47:21 +0800

    dubbo

---
+++ src/main/java/love/moon/dubbo/demo/service/DemoServiceImpl.java
@@ -0,0 +1,18 @@
+package love.moon.dubbo.demo.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by lovemooner on 2017/5/2.
+ */
+@Service
+public class DemoServiceImpl implements DemoService {
+    private static final Logger LOG = LoggerFactory.getLogger(DemoServiceImpl.class);
+    public String sayHello(String name) {
+        LOG.info("Server:receiver name--",name);
+        return "Hello " + name;
+    }
+}
