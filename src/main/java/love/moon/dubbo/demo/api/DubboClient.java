68c779f7dc0a1f8f92d39f809526bd171b6dbe83
ndong211 <ndong211@163.com>
2017-05-13 20:58:49 +0800

    2

--- src/main/java/love/moon/dubbo/demo/api/DubboClient.java
+++ src/main/java/love/moon/dubbo/demo/api/DubboClient.java
@@ -3,44 +3,37 @@ package love.moon.dubbo.demo.api;
 import com.alibaba.dubbo.config.*;
 import love.moon.dubbo.demo.service.DemoService;
 import love.moon.dubbo.demo.service.DemoServiceImpl;
+import org.junit.Test;
+
+import static love.moon.dubbo.demo.api.DubboProvider.ADDRESS;
+import static love.moon.dubbo.demo.api.DubboProvider.APP_NAME;
 
 /**
- * Created by ndong on 2017/5/5.
+ * Created by ndong on 2017/5/13.
  */
 public class DubboClient {
 
     public static void main(String[] args) {
-        // 服务实现
-        DemoService demoService = new DemoServiceImpl();
-
-// 当前应用配置
+        // 当前应用配置
         ApplicationConfig application = new ApplicationConfig();
-        application.setName("xxx");
+        application.setName(APP_NAME);
 
 // 连接注册中心配置
         RegistryConfig registry = new RegistryConfig();
-        registry.setAddress("10.20.130.230:9090");
-        registry.setUsername("aaa");
-        registry.setPassword("bbb");
+        registry.setAddress(ADDRESS);
 
-// 服务提供者协议配置
-        ProtocolConfig protocol = new ProtocolConfig();
-        protocol.setName("dubbo");
-        protocol.setPort(12345);
-        protocol.setThreads(200);
+// 注意：ReferenceConfig为重对象，内部封装了与注册中心的连接，以及与服务提供方的连接
 
-// 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
+// 引用远程服务
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<DemoService>(); // 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
+        reference.setApplication(application);
+        reference.setRegistry(registry); // 多个注册中心可以用setRegistries()
+        reference.setInterface(DemoService.class);
+        reference.setVersion("1.0.0");
 
-// 服务提供者暴露服务配置
-        ServiceConfig<DemoService> service = new ServiceConfig<DemoService>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
-        service.setApplication(application);
-        service.setRegistry(registry); // 多个注册中心可以用setRegistries()
-        service.setProtocol(protocol); // 多个协议可以用setProtocols()
-        service.setInterface(DemoService.class);
-        service.setRef(demoService);
-        service.setVersion("1.0.0");
-
-// 暴露及注册服务
-        service.export();
+// 和本地bean一样使用xxxService
+        DemoService demoService = reference.get(); // 注意：此代理对象内部封装了所有通讯细节，对象较重，请缓存复用
+        String result = demoService.sayHello("server1");
+        System.out.println(result);
     }
 }
